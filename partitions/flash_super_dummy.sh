615fdeb1b1244bd4a58ed275c6833a0e43664e21
Guidix <guidixm@gmail.com>
2024-07-06 17:17:17 +0300

    Revert "lavender: drop super_dummy.img"
    This reverts commit 1546056545b480772668eb977535fc71d4ea13fa.

--- partitions/flash_super_dummy.sh
+++ partitions/flash_super_dummy.sh
@@ -11,4 +11,10 @@ mkdir /tmp/super-mnt
 
 mount $SUPER /tmp/super-mnt 2>/dev/null
 
+if [ "$?" = "0" ]; then
+    echo "Detected stock /system in super partition, flashing super_dummy.img!"
+    umount /tmp/super-mnt
+    dd if=/tmp/super_dummy.img of=$SUPER
+fi
+
 rmdir /tmp/super-mnt
